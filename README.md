e07fa868698ef8e4456b7a926228d5fa3c68d92d
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2022-08-21 21:01:01 +0300

    compete Arabic Tashkeel for all governance and cities

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Yemen-info
+
 Here is Json file for the most information for Yemen programmatically
 
-
 ## TODO
+
 We can learn a lot from [this project](https://github.com/dr5hn/countries-states-cities-database)
 
-- [ ] Add Tashkeel in addition to Arabic names. we can use these automatic service at the beginning: [tashkeel](https://tashkeel.alsharekh.org/), [modaqiq](https://dictionary.alc.ae/modaqiq), [mishkal](https://tahadz.com/mishkal), and [tashkil](http://www.7koko.com/apps/tashkil/index.php)
+- [ x ] Add Tashkeel in addition to Arabic names. we can use these automatic service at the beginning: [modaqiq](https://dictionary.alc.ae/modaqiq), then we add Tashkeel manually based on people who know them.
 - [ ] Add polygon for map coordinates
-- [ ] check more for the correctness of the information
+- [ ] Add latitude and longitude for map coordinates in governance / cities centers
 - [ ] add script to convert json file automatically to: csv, SQLite, and other useful formats
 - [ ] add descriptive and better Readme file
 - [ ] Add general information about Yemen (area, location, etc)
@@ -15,3 +16,20 @@ We can learn a lot from [this project](https://github.com/dr5hn/countries-states
 - [ ] add custom cspell dictionary for spell checkers, to ignore Yemeni cities/ governance's names
 - [ ] add automatic script to add all cities/governance's names to cspell dictionary
 
+- [ ] add useful and joyful JS map like [this](https://yemenlg.org/ar/)
+
+- [ ] add more info links /  and backup link to the same site in Archive.org links from these websites:
+    1. https://yemen-nic.info/
+    2. https://yemenlg.org/ar/resources-by-governorate/
+- [ ] add id for every city/governance
+
+- [] numbers phone prefix
+    https://sites.google.com/site/yemenfonweb/keyyecty
+    https://ar.wikipedia.org/wiki/%D8%A3%D8%B1%D9%82%D8%A7%D9%85_%D8%A7%D9%84%D9%87%D8%A7%D8%AA%D9%81_%D9%81%D9%8A_%D8%A7%D9%84%D9%8A%D9%85%D9%86
+
+- Yemen cars prefex
+    https://ye.opensooq.com/ar/%D8%B5%D9%86%D8%B9%D8%A7%D8%A1/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%88%D9%85%D8%B1%D9%83%D8%A8%D8%A7%D8%AA/%D9%84%D9%88%D8%AD%D8%A7%D8%AA-%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%85%D9%85%D9%8A%D8%B2%D8%A9-%D9%84%D9%84%D8%A8%D9%8A%D8%B9
+
+## Resources
+- [Yemen](https://en.wikipedia.org/wiki/Yemen)
+- [modaqiq](https://dictionary.alc.ae/modaqiq) for automatic Tashkeel and spell checker
--- automated-tasks.py
+++ automated-tasks.py
@@ -22,22 +22,26 @@ def remove_duplication():
     json_file = read_json_file('./yemen.json')
     # TODO
 
+
 def sort_json_by_governorate_name(file_path):
     json_data = read_json_file(file_path)
     workflows = json_data["Governorates"]
 
     sorted_data = sorted(workflows, key=lambda d: d["name_en"])
 
-    write_json_file(file_path, sorted_data) # TODO keep the original structure like "Governorates"
+    write_json_file(file_path, sorted_data)  # TODO keep the original structure like "Governorates"
     # TODO
     return 'Complete sorting by `governorate name_en` name'
+
 
 def sort_governorates_by_name_ar(file_path):
     # TODO
     return 'Complete sorting by `governorate name_en` name'
 
+
 def sort_governorate_cities_by_name_en(file_path):
     return 'Complete sorting by `governorate name_en` name'
+
 
 def sort_governorate_cities_by_name_ar(file_path):
     return 'Complete sorting by `governorate name_ar` name'
@@ -62,21 +66,88 @@ def convert_json_to_yaml():
     json_file = read_json_file('./yemen.json')
     # TODO
 
+
 def add_all_yemeni_names_to_custom_dictionary():
     json_file = read_json_file('./yemen.json')
     # TODO
+
 
 def add_tashkeel_to_governorates_and_cities():
     json_file = read_json_file('./yemen.json')
     # TODO
     # loop around every governorate and add name_ar_tashkeel
     for gov in json_file['governorates']:
-        print(gov)
+        # add name_ar_tashkeel to governorate obj
+        gov['name_ar_tashkeel'] = gov['name_ar']
+        # loop around every city in governorate and add name_ar_tashkeel
+        for city in gov['cities']:
+            city['name_ar_tashkeel'] = city['name_ar']
+
+    write_json_file('./yemen2.json', json_file)
 
 
-    # and loop around every city and add name_ar_tashkeel
+def get_all_governorates_cities_name_ar_tashkeel():
+    gov_names = []
+    cities_names = []
+
+    json_file = read_json_file('./yemen2.json')
+    for gov in json_file['governorates']:
+        gov_names.append({'name_ar_tashkeel': gov['name_ar_tashkeel'], 'name_en': gov['name_en']})
+        for city in gov['cities']:
+            cities_names.append({'name_ar_tashkeel': city['name_ar_tashkeel'], 'name_en': city['name_en']})
+
+    write_json_file('./yemen3.json', {'governorates': gov_names, 'cities': cities_names})
+
+
+def change_after_google_sheet_to_json():
+
+    json_file = read_json_file('./correct_gov_needs_format.json')
+
+    for gov in json_file:
+        # if there is governorates__name_en
+        # add inner obj for cities
+
+        # if 'ct'
+        # gov['cities'] = []
+        # if 'governorates__name_en' in gov:
+        #     gov['cities'] = [
+        #         {
+        #             'name_en': gov['governorates__cities__name_en'],
+        #             'name_ar': gov['governorates__cities__name_ar'],
+        #             'name_ar_tashkeel': gov['governorates__cities__name_ar_tashkeel']
+        #         }
+        #     ]
+        #     # remove governorates__name_en
+        #     del gov['governorates__cities__name_en']
+        #     del gov['governorates__cities__name_ar']
+        #     del gov['governorates__cities__name_ar_tashkeel']
+
+        # if there is no governorates__name_en
+        # add this object to the previous cities array
+            # go back until you find object with governorates__name_en
+            # add this object to the previous cities array
+
+
+
+        # if 'governorates__cities__name_en' in gov:
+            # gov['cities'].append({'name_en': gov[s
+
+            # gov['cities'].append(
+            #     {
+            #         'name_en': gov['governorates__cities__name_en'],
+            #         'name_ar': gov['governorates__cities__name_ar'],
+            #         'name_ar_tashkeel': gov['governorates__cities__name_ar_tashkeel']
+            #     }
+            # )
+
+            # del gov['governorates__cities__name_en']
+            # del gov['governorates__cities__name_ar']
+            # del gov['governorates__cities__name_ar_tashkeel']
+
+    write_json_file('./correct2.json', json_file)
+
 
 ################################
 # Using Functions
 ################################
-add_tashkeel_to_governorates_and_cities()
+change_after_google_sheet_to_json()
