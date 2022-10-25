d88a3dc21a5d8f9a2df56645b8489eaf2074b6af
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-10-25 18:31:56 +0300

    working on completing the conversion from json to SQL

--- utilities/json2sql.py
+++ utilities/json2sql.py
@@ -54,13 +54,14 @@ def get_values(items, table_name, reference_table, reference_table_id):
     if table_name not in nested_tables:
         nested_tables.update({table_name: []})
     for item in items:
-        item.update({reference_table: reference_table_id, **item})
+        # print({reference_table: reference_table_id, **item})
+        item = {f"{singular_dict.get(reference_table)}_id": reference_table_id, **item}
         nested_tables.get(table_name).append(item)
 
 
 def _insert_into_from_list(another_tables, insert_into, json_data, reference_table, reference_table_id, table_name,
                            cols):
-    insert_into += f"{str(reference_table_id) if reference_table else ''}"
+    insert_into += f"({str(reference_table_id) if reference_table else ''}"
     for index, items in enumerate(json_data):
         if "id" not in items:
             insert_into += f", {index + 1}"
@@ -68,7 +69,7 @@ def _insert_into_from_list(another_tables, insert_into, json_data, reference_tab
             if key not in another_tables:
                 insert_into += f", {repr(items.get(key))}"
             else:
-                get_values(items.get(key), key, table_name, items.get("id", index+1))
+                get_values(items.get(key), key, table_name, items.get("id", index + 1))
         insert_into += f"),\n({str(reference_table_id)}" if 1 <= index + 1 < len(json_data) else ")\n"
         # for key in another_tables:
         #     sql = _insert_into(items.get(key), key, table_name, items.get("id"), sql, cols, another_tables)
@@ -78,12 +79,12 @@ def _insert_into_from_list(another_tables, insert_into, json_data, reference_tab
 
 def _insert_into(json_data: Union[list, dict], table_name: str, reference_table: str, reference_table_id: int, sql: str,
                  cols: str, nested_tables: list):
-    insert_into = f"INSERT INTO `{table_name}`\n({cols})\nVALUES\n("
+    insert_into = f"INSERT INTO `{table_name}`\n({cols})\nVALUES\n"
     if isinstance(json_data, list):
         insert_into = _insert_into_from_list(nested_tables, insert_into, json_data, reference_table,
                                              reference_table_id, table_name, cols)
     elif isinstance(json_data, dict):
-        insert_into += f"{str(reference_table_id) + ', ' if reference_table else ''}"
+        insert_into += f"({str(reference_table_id) + ', ' if reference_table else ''}"
         if "id" not in json_data:
             insert_into += f"{reference_table_id if reference_table_id else 1}"  # TODO: here supposed to be the primary key.
             # TODO: if table is created call get_values() function to get (1,2,) values syntax only of the rows
@@ -103,12 +104,27 @@ def _insert_into(json_data: Union[list, dict], table_name: str, reference_table:
 
 def create_table(json_data: dict, table_name: str = "countries", reference_table: str = None,
                  reference_table_id: int = 1):
+    # TODO: get the number max level in the json
     overall = ""
     data = json_data[0] if isinstance(json_data, list) else json_data
+    # TODO: Abdullah why don't you check here:
+    #  there should be an external/general dictionary to hold created tables w their sql, cols, and another tables.
+    #  for every item in the json data list, if the item doesn't has a created table,
+    #  then call create table, else just call the get rows values.
+    # TODO (INCOMPLETE)
     sql, cols, nested_tables = _create_table(json_data=data, table_name=table_name, reference_table=reference_table)
+    created_tables.update({table_name: {
+        "sql": sql,
+        "cols": cols,
+        "nested_tables": nested_tables
+    }})
     sql = _insert_into(json_data=json_data, table_name=table_name, reference_table=reference_table,
                        reference_table_id=reference_table_id, sql=sql, cols=cols, nested_tables=nested_tables)
-    created_tables.update({table_name: "👍"})
+    created_tables.update({table_name: {
+        "sql": sql
+    }})
+    # if "districts" in nested_tables:
+    #     nested_tables.remove("districts")
     for table in nested_tables:
         overall += create_table(data.get(table), table, table_name, data.get("id", 1))
 
@@ -121,6 +137,7 @@ def json_to_sql(json_data: dict):
           "CREATE DATABASE IF NOT EXISTS `countries`;\n" \
           "USE `countries`;\n\n"
     sql += create_table(json_data=json_data)
-    with open("nested_tables.json", "w") as file:
-        file.write(json.dumps(nested_tables))
+    with open("nested_tables.json", "w", encoding="utf-16") as file:
+        file.write(json.dumps(nested_tables, ensure_ascii=False, indent=2))
+    print(created_tables)
     return sql
