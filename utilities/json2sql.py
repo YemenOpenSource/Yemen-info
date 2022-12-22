bf5cdebd442641d6b1f2b2f77e2fd0ee621d6dee
Abdullah Nasser <abdullah.nasser@testifi.io>
2022-12-22 11:27:16 +0300

    json to sql formatted the file

--- utilities/json2sql.py
+++ utilities/json2sql.py
@@ -27,19 +27,21 @@ def _get_list_cols_nested_tables(table_name: str, json_data: dict):
 
 
 def _create_table(cols: list, table_name: str = "countries", reference_table: str = None):
+    _table = singular_dict.get(table_name, table_name.lower())
+    _ref_table = singular_dict.get(reference_table, reference_table.lower()) if reference_table else ""
     create_query = f"DROP TABLE IF EXISTS `{table_name}`;\nCREATE TABLE IF NOT EXISTS `{table_name}` (\n\t"
-    create_query += f"`{singular_dict.get(table_name, table_name.lower())}_id` bigint UNSIGNED PRIMARY KEY NOT NULL,"
+    create_query += f"`{_table}_id` bigint UNSIGNED PRIMARY KEY NOT NULL,"
     if reference_table:
-        create_query += f"\n\t`{singular_dict.get(reference_table, reference_table.lower())}_id` bigint UNSIGNED NOT NULL,"
+        create_query += f"\n\t`{_ref_table}_id` bigint UNSIGNED NOT NULL,"
     for col in cols:
         create_query += f"\n\t`{col}` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL" \
                         f"{',' if col != cols[-1] else ''}"
     if reference_table:
-        create_query += f",\n\tCONSTRAINT fk_{singular_dict.get(reference_table)}_{table_name} " \
-                        f"FOREIGN KEY (`{singular_dict.get(reference_table)}_id`) " \
-                        f"REFERENCES {reference_table}(`{singular_dict.get(reference_table)}_id`)"
+        create_query += f",\n\tCONSTRAINT fk_{_ref_table}_{_table} " \
+                        f"FOREIGN KEY (`{_ref_table}_id`) " \
+                        f"REFERENCES {reference_table}(`{_ref_table}_id`)"
     create_query += "\n)\tENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;\n"
-    _cols = f"`{singular_dict.get(table_name, table_name.lower())}_id`, {'`' + singular_dict.get(reference_table, reference_table.lower()) + '_id`, ' if reference_table else ''}"
+    _cols = f"`{_table}_id`, {'`' + _ref_table + '_id`, ' if reference_table else ''}"
     for col in cols:
         _cols += f"`{col}`, "
     cols = _cols.strip(", ")
